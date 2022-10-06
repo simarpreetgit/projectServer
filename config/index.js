288cfed9d1ee7a56466494b0880ce71579156dd8
Simarpreet <preet091989@gmail.com>
2022-10-06 18:12:54 +0200

    deplychanges

--- config/index.js
+++ config/index.js
@@ -23,7 +23,8 @@ module.exports = (app) => {
   app.use(logger("dev"));
 
   app.use(   
-     cors({      origin: ["http://localhost:3000"],    })  );
+     cors({      origin: ["http://localhost:3000","https://singh-restro.netlify.app/"], 
+       })  );
   // To have access to `body` property in the request
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
